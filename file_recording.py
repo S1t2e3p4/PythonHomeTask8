daa59f4fbc7cecb32372ae1393f52e7a3b575fea
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-02-11 15:11:46 +0300

    Прописал функции записи данных в файл.

--- file_recording.py
+++ file_recording.py
@@ -1,7 +1,6 @@
 import logger as log
 from datetime import datetime as dt
 
-
 def record_to_file(data):
     with open('School_database.txt', 'a') as file:
         time = dt.now().strftime('%d/%m/%Y %H:%M')
@@ -9,7 +8,6 @@ def record_to_file(data):
         file.write(str(data))
         file.write('\n')
     log.log_input_data('Database updated in .txt file')
-
 
 def clear_file(filename):
     file = open(filename, 'w')
